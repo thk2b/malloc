77d5e0be3aeda1753a9f9a33d3a4730e4f2a438d
Theo <theokobb@gmail.com>
2018-12-20 01:45:08 -0800

    add scripts to inject/reset library

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 export DYLD_LIBRARY_PATH=.
 export DYLD_INSERT_LIBRARIES=libft_malloc.so
 export DYLD_FORCE_FLAT_NAMESPACE=1
 export LD_LIBRARY_PATH=.
 export LD_INSERT_LIBRARIES=libft_malloc.so
 export LD_FORCE_FLAT_NAMESPACE=1
-$@
+export LD_PRELOAD=./libft_malloc.so
