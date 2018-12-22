86b79fc16c8cba0faa1143c8291d882152f17f59
Theo <theokobb@gmail.com>
2018-12-22 03:24:18 -0800

    write realloc, debug

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -1,8 +1,16 @@
 #include <ft_malloc.h>
+#include <string.h>
 
 extern void	*realloc(void *ptr, size_t size)
 {
-	(void)ptr;
-	(void)size;
-	return (NULL);
+	char	*new_ptr;
+	t_block	*block;
+	
+	if ((new_ptr = malloc(size)) == NULL)
+		return (NULL);
+	if ((block = find_block(ptr)) == NULL)
+		return (new_ptr);
+	memmove(new_ptr, ptr, block->size);
+	free(ptr);
+	return (new_ptr);
 }
