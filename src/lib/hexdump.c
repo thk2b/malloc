bcd21a32a2a9a4bb9d79edbf02af6463775f76bc
thk2b <theokobb@gmail.com>
2019-02-06 07:42:50 -0800

    final norming

--- src/lib/hexdump.c
+++ src/lib/hexdump.c
@@ -6,17 +6,38 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 07:33:24 by tkobb             #+#    #+#             */
-/*   Updated: 2019/02/05 07:35:36 by tkobb            ###   ########.fr       */
+/*   Updated: 2019/02/06 07:42:04 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lib.h>
 
-void	hexdump(void *ptr, size_t size, char *color)
+static inline void	dumb(unsigned char *d, size_t i)
+{
+	#ifndef HEXDUMP_BG
+	put_str(1, HEXDUMP_ADDRESS);
+	put_hex(1, (size_t)(d + i), 0);
+	put_str(1, " ");
+	put_str(1, RESET);
+	#endif
+}
+
+static inline void	dump(unsigned char *d, size_t i)
+{
+	unsigned char	n;
+
+	n = d[i];
+	i += 1;
+	put_hex(1, (size_t)n, 0);
+	if (n < 16)
+		put_str(1, "0");
+	put_str(1, " ");
+}
+
+void				hexdump(void *ptr, size_t size, char *color)
 {
 	static size_t	count = 0;
 	size_t			i;
-	unsigned char	n;
 	unsigned char	*d;
 
 	if (ptr == NULL)
@@ -29,24 +50,10 @@ void	hexdump(void *ptr, size_t size, char *color)
 	while (i < size)
 	{
 		if (count == 0)
-		{
-			#ifndef HEXDUMP_BG
-			put_str(1, HEXDUMP_ADDRESS);
-			put_hex(1, (size_t)(d + i), 0);
-			put_str(1, " ");
-			put_str(1, RESET);
-			#endif
-		}
+			dumb(d, i);
 		put_str(1, color);
 		while (i < size && count++ < HEXDUMP_WIDTH)
-		{
-			n = d[i];
-			i += 1;
-			put_hex(1, (size_t)n, 0);
-			if (n < 16)
-				put_str(1, "0");
-			put_str(1, " ");
-		}
+			dump(d, i++);
 		if (i >= size)
 			break ;
 		count = 0;
@@ -54,10 +61,3 @@ void	hexdump(void *ptr, size_t size, char *color)
 	}
 	put_str(1, RESET);
 }
-
-#ifdef TEST
-int		main(int ac, char **av)
-{
-	hexdump((void*)av, ac * sizeof(char*), "");
-}
-#endif
